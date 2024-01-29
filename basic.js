6fa8567700185a53243191af919e293302c9735b
Apple <apple@Dhruvs-MAC2.local>
2024-01-29 23:37:54 +0530

    v 0.2

---
+++ basic.js
@@ -0,0 +1,56 @@
+
+// var functionScopedVar ="i am function-scoped";
+// function exampleFunction(){
+
+ //    var functionScopedVar = 4;
+
+//     if(true){
+       // console.log(functionScopedVar);
+//         var functionScopedVar = 5;
+//         console.log(functionScopedVar); // "i am function-stopped"-- it will run here
+//     }
+//     console.log(functionScopedVar); // error here
+
+// }
+// exampleFunction();
+// console.log(functionScopedVar);//function ke bhr nh hoga
+
+
+// let star = {
+//     name: "alice",
+//     age: 30,
+//     isstudent: false
+    
+// }
+// console.log(star.name);
+// console.log(star.age);
+// console.log(star.isstudent);
+// console.log(star);
+
+// let count = 0;
+// do{
+//     console.log(count);
+//     count++;
+
+// }while (count < 5);
+
+// let person = {name:"Dhruv",age:20,occupation:"Developer"};
+// let target="name";
+
+// for(let key in person){
+
+//         console.log(person[key]);
+    
+// }
+
+// let car={
+//     make
+// }
+
+// logical oprators
+// let p=true;
+// let q=false;
+// console.log(!p);
+// console.log(p||q);
+// console.log(p&&q);
+
